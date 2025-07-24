38255d76aeb509912d4b7ee2693baa0570c2db08
Atiq <matiq83@users.noreply.github.com>
2025-07-24 18:28:51 +0500

    Revert "remove description"
    This reverts commit b79cf10fe4961ded6fe0dcab5e525f07288d3804.

--- api/tables/tasks.sql
+++ api/tables/tasks.sql
@@ -1,6 +1,7 @@
 CREATE TABLE tasks (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
+    description TEXT,
     status ENUM('to-do','in-progress', 'done') DEFAULT 'to-do',
     sort_order INT DEFAULT 0,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
